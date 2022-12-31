6bfc096c814c496b1cf0d8541f7a746d236ad3f7
Nsubuga Derrick <rite2drk@gmail.com>
2022-12-31 11:17:59 +0300

    Package with electron forge

---
+++ forge.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  packagerConfig: {
+    icon:'assets/icons/win/icon.ico',
+    setupIcon: 'assets/icons/win/icon.ico'
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-zip'
+    },
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+    "publishers": [
+        {
+          "name": "@electron-forge/publisher-github",
+          "config": {
+            "repository": {
+              "owner": "drkNsubuga",
+              "name": "electron-publish-example",
+              "draft": true
+            }
+          }
+        }
+      ]
+};
