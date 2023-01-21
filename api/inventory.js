db0c60a03a0cd12ec7a02c34929d6678838486c8
Nsubuga Derrick <rite2drk@gmail.com>
2023-01-21 17:18:21 +0300

    Fixed sec issue

--- api/inventory.js
+++ api/inventory.js
@@ -1,190 +1,118 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const bodyParser = require("body-parser");
-const Datastore = require("nedb");
-const async = require("async");
-const fileUpload = require('express-fileupload');
+const app = require( "express")();
+const server = require( "http" ).Server( app );
+const bodyParser = require( "body-parser" );
+const Datastore = require( "nedb" );
 const multer = require("multer");
+const fileUpload = require('express-fileupload');
 const fs = require('fs');
-const sanitize = require('sanitize-filename');
-// set up rate limiter: maximum of five requests per minute
+const path = require('path');
 var RateLimit = require('express-rate-limit');
+
 var limiter = RateLimit({
   windowMs: 1*60*1000, // 1 minute
   max: 5
 });
 
-
 app.use(limiter);
 
 const storage = multer.diskStorage({
-    destination: process.env.APPDATA + '/POS/uploads',
-    filename: function (req, file, callback) {
+    destination:  path.join(process.env.APPDATA,'POS','uploads'),
+    filename: function(req, file, callback){
         callback(null, Date.now() + '.jpg'); // 
     }
 });
 
+let upload = multer({storage: storage});
 
-let upload = multer({ storage: storage });
-
-app.use(bodyParser.json());
-
+app.use( bodyParser.json() );
 
 module.exports = app;
 
-
-let inventoryDB = new Datastore({
-    filename: process.env.APPDATA + "/POS/server/databases/inventory.db",
+ 
+let settingsDB = new Datastore( {
+    filename: path.join(process.env.APPDATA,"POS","server","databases","settings.db"),
     autoload: true
-});
-
-
-inventoryDB.ensureIndex({ fieldName: '_id', unique: true });
-
-
-app.get("/", function (req, res) {
-    res.send("Inventory API");
-});
+} );
 
 
 
-app.get("/product/:productId", function (req, res) {
-    if (!req.params.productId) {
-        res.status(500).send("ID field is required.");
-    } else {
-        inventoryDB.findOne({
-            _id: parseInt(req.params.productId)
-        }, function (err, product) {
-            res.send(product);
-        });
-    }
-});
+app.get( "/", function ( req, res ) {
+    res.send( "Settings API" );
+} );
 
 
+  
+app.get( "/get", function ( req, res ) {
+    settingsDB.findOne( {
+        _id: 1
+}, function ( err, docs ) {
+        res.send( docs );
+    } );
+} );
 
-app.get("/products", function (req, res) {
-    inventoryDB.find({}, function (err, docs) {
-        res.send(docs);
-    });
-});
-
-
-
-app.post("/product", upload.single('imagename'), function (req, res) {
+ 
+app.post( "/post", upload.single('imagename'), function ( req, res ) {
 
     let image = '';
 
-    if (req.body.img != "") {
-        image = req.body.img;
+    if(req.body.img != "") {
+        image = req.body.img;       
     }
 
-    if (req.file) {
-        image = req.file.filename;
+    if(req.file) {
+        image = req.file.filename;  
     }
 
-
-    if (req.body.remove == 1) {
-        const path = './resources/app/public/uploads/product_image/' + req.body.img;
+    if(req.body.remove == 1) {
+        const imgPath = path.join(process.env.APPDATA,"POS","uploads",req.body.img);
         try {
-            fs.unlinkSync(sanitize(path))
-        } catch (err) {
-            console.error(err)
+          fs.unlinkSync(imgPath)
+        } catch(err) {
+          console.error(err)
         }
 
-        if (!req.file) {
+        if(!req.file) {
             image = '';
         }
+    } 
+    
+  
+    let Settings = {  
+        _id: 1,
+        settings: {
+            "app": req.body.app,
+            "store": req.body.store,
+            "address_one": req.body.address_one,
+            "address_two":req.body.address_two,
+            "contact": req.body.contact,
+            "tax": req.body.tax,
+            "symbol": req.body.symbol,
+            "percentage": req.body.percentage,
+            "charge_tax": req.body.charge_tax,
+            "footer": req.body.footer,
+            "img": image
+        }       
     }
 
-    let Product = {
-        _id: parseInt(req.body.id),
-        barcode: parseInt(req.body.barcode),
-        expirationDate: req.body.expirationDate == "" ? "29/08/2030" : req.body.expirationDate,
-        profit: parseInt(req.body.profit),
-        price: req.body.price,
-        category: req.body.category,
-        quantity: req.body.quantity == "" ? 0 : req.body.quantity,
-        name: req.body.name,
-        stock: req.body.stock == "on" ? 0 : 1,
-        img: image
-    }
-
-    if (req.body.id == "") {
-        Product._id = Math.floor(Date.now() / 1000);
-        inventoryDB.insert(Product, function (err, product) {
-            if (err) res.status(500).send(err);
-            else res.send(product);
+    if(req.body.id == "") { 
+        settingsDB.insert( Settings, function ( err, settings ) {
+            if ( err ) res.status( 500 ).send( err );
+            else res.send( settings );
         });
     }
-    else {
-        inventoryDB.update({
-            _id: parseInt(req.body.id)
-        }, Product, {}, function (
+    else { 
+        settingsDB.update( {
+            _id: 1
+        }, Settings, {}, function (
             err,
             numReplaced,
-            product
+            settings
         ) {
-            if (err) res.status(500).send(err);
-            else res.sendStatus(200);
-        });
-
+            if ( err ) res.status( 500 ).send( err );
+            else res.sendStatus( 200 );
+        } );
     }
 
 });
 
-
-
-
-app.delete("/product/:productId", function (req, res) {
-    inventoryDB.remove({
-        _id: parseInt(req.params.productId)
-    }, function (err, numRemoved) {
-        if (err) res.status(500).send(err);
-        else res.sendStatus(200);
-    });
-});
-
-
-
-app.post("/product/sku", function (req, res) {
-    var request = req.body;
-    inventoryDB.findOne({
-        barcode: parseInt(request.skuCode)
-    }, function (err, product) {
-        res.send(product);
-    });
-});
-
-
-
-
-app.decrementInventory = function (products) {
-
-    async.eachSeries(products, function (transactionProduct, callback) {
-        inventoryDB.findOne({
-            _id: parseInt(transactionProduct.id)
-        }, function (
-            err,
-            product
-        ) {
-
-            if (!product || !product.quantity) {
-                callback();
-            } else {
-                let updatedQuantity =
-                    parseInt(product.quantity) -
-                    parseInt(transactionProduct.quantity);
-
-                inventoryDB.update({
-                    _id: parseInt(product._id)
-                }, {
-                    $set: {
-                        quantity: updatedQuantity
-                    }
-                }, {},
-                    callback
-                );
-            }
-        });
-    });
-};
+ 
